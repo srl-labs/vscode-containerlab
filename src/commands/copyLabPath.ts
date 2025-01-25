a7c058464f5dd1e82f44f1e1c8952bf1b3cfe684
Kaelem Chandra <kc@kaelem.net>
2025-01-25 20:09:41 +1300

    Allow non-running labs to be copied correctly.

--- src/commands/copyLabPath.ts
+++ src/commands/copyLabPath.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import * as utils from "../utils";
 import { ContainerlabNode } from "../containerlabTreeDataProvider";
 
 export function copyLabPath(node: ContainerlabNode) {
@@ -12,8 +13,10 @@ export function copyLabPath(node: ContainerlabNode) {
       vscode.window.showErrorMessage('No labPath found.');
       return;
     }
+
+    const labName = node.details?.labName ? node.details?.labName : utils.getRelativeFolderPath(labPath);
     
     vscode.env.clipboard.writeText(labPath).then(() => {
-        vscode.window.showInformationMessage(`Copied file path of ${node.details?.labName} to clipboard.`)
+        vscode.window.showInformationMessage(`Copied file path of ${labName} to clipboard.`)
     });
 }
