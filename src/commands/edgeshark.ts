c81d2bc4215ab2cb3f667d26e59448422fab2808
Kaelem Chandra <kc@kaelem.net>
2025-01-30 00:35:05 +1300

    Change edgeshark install terminal names

--- src/commands/edgeshark.ts
+++ src/commands/edgeshark.ts
@@ -3,11 +3,11 @@ import { execCommandInTerminal } from "./command";
 export async function installEdgeshark() {
     execCommandInTerminal("curl -sL \
 https://github.com/siemens/edgeshark/raw/main/deployments/wget/docker-compose.yaml \
-| DOCKER_DEFAULT_PLATFORM= docker compose -f - up -d", "Edgeshark installation");
+| DOCKER_DEFAULT_PLATFORM= docker compose -f - up -d", "Edgeshark Installation");
 }
 
 export async function uninstallEdgeshark() {
     execCommandInTerminal("curl -sL \
 https://github.com/siemens/edgeshark/raw/main/deployments/wget/docker-compose.yaml \
-| DOCKER_DEFAULT_PLATFORM= docker compose -f - down", "Edgeshark uninstallation");
+| DOCKER_DEFAULT_PLATFORM= docker compose -f - down", "Edgeshark Uninstallation");
 }
