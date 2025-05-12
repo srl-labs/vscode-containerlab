deab5667e2ec8088774286a5ce827a7bcda4422a
Kaelem Chandra <kc@kaelem.net>
2025-05-13 10:22:25 +1200

    Improve check for deployed lab. (Grab inspect data once only)

--- src/treeView/localLabsProvider.ts
+++ src/treeView/localLabsProvider.ts
@@ -1,84 +1,129 @@
 import * as vscode from "vscode"
 import * as utils from "../utils"
-import {CtrStateIcons, ClabLabTreeNode} from "./common";
+import * as c from "./common";
 import path = require("path");
+import { exec } from "child_process";
+import { promisify } from "util";
+import { outputChannel } from "../extension";
 
+const execAsync = promisify(exec);
+
+const WATCHER_GLOB_PATTERN = "{**/*.clab.yml,**/*.clab.yaml}"
 const CLAB_GLOB_PATTERN = "{**/*.clab.yml,**/*.clab.yaml}";
 const IGNORE_GLOB_PATTERN = "**/node_modules/**";
 
-export class LocalLabTreeDataProvider implements vscode.TreeDataProvider<ClabLabTreeNode | undefined> {
-  private _onDidChangeTreeData = new vscode.EventEmitter<void | ClabLabTreeNode | undefined>();
-  readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
+export class LocalLabTreeDataProvider implements vscode.TreeDataProvider<c.ClabLabTreeNode | undefined> {
+    private _onDidChangeTreeData = new vscode.EventEmitter<void | c.ClabLabTreeNode | undefined>();
+    readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
 
-  // glob everything so we can events when folders are deleted
-  private watcher = vscode.workspace.createFileSystemWatcher("**", false, false, false);
+    private watcher = vscode.workspace.createFileSystemWatcher(WATCHER_GLOB_PATTERN, false, false, false);
 
-  constructor(private context: vscode.ExtensionContext) {
-    this.watcher.onDidCreate(() => {this.refresh();});
-    this.watcher.onDidDelete(() => {this.refresh();});
-    this.watcher.onDidChange(() => {this.refresh();});
-  }
-
-  refresh(): void {
-    this._onDidChangeTreeData.fire();
-  }
-
-  getTreeItem(element: ClabLabTreeNode): vscode.TreeItem {
-    return element;
-  }
-
-  // Populate the tree
-  async getChildren(): Promise<any> {
-    return this.discoverLabs();
-  }
-
-  private async discoverLabs(): Promise<ClabLabTreeNode[] | undefined> {
-    console.log("[LocalTreeDataProvider]:\tDiscovering labs...");
-
-    const uris = await vscode.workspace.findFiles(CLAB_GLOB_PATTERN, IGNORE_GLOB_PATTERN);
-
-    // empty tree if no files were discovered
-    if (!uris.length) {
-      return undefined;
+    constructor(private context: vscode.ExtensionContext) {
+        this.watcher.onDidCreate(() => { this.refresh(); });
+        this.watcher.onDidDelete(() => { this.refresh(); });
+        this.watcher.onDidChange(() => { this.refresh(); });
     }
 
-    const labs: Record<string, ClabLabTreeNode> = {};
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
 
-    uris.forEach((uri) => {
-      const normPath = utils.normalizeLabPath(uri.fsPath);
-      if (!labs[normPath]) {
-        const labNode = new ClabLabTreeNode(
-          path.basename(uri.fsPath),
-          vscode.TreeItemCollapsibleState.None,
-          {
-            relative: uri.fsPath,
-            absolute: normPath
-          },
-          undefined,
-          undefined,
-          "containerlabLabUndeployed"
-        );
+    getTreeItem(element: c.ClabLabTreeNode): vscode.TreeItem {
+        return element;
+    }
 
-        labNode.description = utils.getRelLabFolderPath(uri.fsPath);
+    // Populate the tree
+    async getChildren(): Promise<any> {
+        return this.discoverLabs();
+    }
 
-        const icon = this.getResourceUri(CtrStateIcons.UNDEPLOYED);
-        labNode.iconPath = { light: icon, dark: icon };
+    private async discoverLabs(): Promise<c.ClabLabTreeNode[] | undefined> {
+        console.log("[LocalTreeDataProvider]:\tDiscovering labs...");
 
-        labs[normPath] = labNode;
-      }
-    });
+        const uris = await vscode.workspace.findFiles(CLAB_GLOB_PATTERN, IGNORE_GLOB_PATTERN);
 
-    // return sorted array of ClabLabTreeNode(s)
-    return Object.values(labs).sort(
-        (a, b) => {
-            // sort based on labPath as it has to be unique
-            return a.labPath.absolute.localeCompare(b.labPath.absolute);
+        // empty tree if no files were discovered
+        if (!uris.length) {
+            return undefined;
         }
-    );
-  }
 
-  // getResourceUri remains unchanged
-  private getResourceUri(resource: string) {
-    return vscode.Uri.file(this.context.asAbsolutePath(path.join("resources", resource)));
-  }
+        const labs: Record<string, c.ClabLabTreeNode> = {};
+
+        const labPaths = await this.getInspectData();
+
+        uris.forEach((uri) => {
+            const normPath = utils.normalizeLabPath(uri.fsPath);
+
+            if (!labs[normPath] && !(labPaths?.has(normPath))) {
+                const labNode = new c.ClabLabTreeNode(
+                    path.basename(uri.fsPath),
+                    vscode.TreeItemCollapsibleState.None,
+                    {
+                        relative: uri.fsPath,
+                        absolute: normPath
+                    },
+                    undefined,
+                    undefined,
+                    undefined,
+                    "containerlabLabUndeployed"
+                );
+
+                labNode.description = utils.getRelLabFolderPath(uri.fsPath);
+
+                const icon = vscode.ThemeIcon.File;
+                labNode.iconPath = icon;
+                // labNode.iconPath = { light: icon, dark: icon };
+
+                labs[normPath] = labNode;
+            }
+        });
+
+        // return sorted array of c.ClabLabTreeNode(s)
+        return Object.values(labs).sort(
+            (a, b) => {
+                // sort based on labPath as it has to be unique
+                return a.labPath.absolute.localeCompare(b.labPath.absolute);
+            }
+        );
+    }
+
+    // getResourceUri remains unchanged
+    private getResourceUri(resource: string) {
+        return vscode.Uri.file(this.context.asAbsolutePath(path.join("resources", resource)));
+    }
+
+    // Parse clab inspect data and return a set of absolute labPaths.
+    // Used to check if a locally discovered lab is deployed or not.
+    private async getInspectData() {
+        const config = vscode.workspace.getConfiguration("containerlab");
+        const runtime = config.get<string>("runtime", "docker");
+
+        const cmd = `${utils.getSudo()}containerlab inspect -r ${runtime} --all --format json 2>/dev/null`;
+
+        let clabStdout;
+        try {
+          const { stdout } = await execAsync(cmd);
+          clabStdout = stdout;
+        } catch (err) {
+          throw new Error(`Could not run ${cmd}.\n${err}`);
+        }
+    
+        if (!clabStdout) {
+          return undefined;
+        }
+    
+        const parsedData = JSON.parse(clabStdout);
+
+        const labPaths = new Set<string>();
+
+        for (const [key, value] of Object.entries(parsedData)) {
+            if(value instanceof Array) {
+                labPaths.add(value[0]['absLabPath']);
+            }
+        }
+
+        return labPaths;
+
+    }
+
 }
