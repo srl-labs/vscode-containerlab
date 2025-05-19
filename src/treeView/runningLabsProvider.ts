36c1f6b442b0769053b698e398cdb3535b416a91
flosch62 <schwarz.flori.88@gmail.com>
2025-05-19 11:34:51 +0200

    Do not hide in the else

--- src/treeView/runningLabsProvider.ts
+++ src/treeView/runningLabsProvider.ts
@@ -140,7 +140,7 @@ export class RunningLabTreeDataProvider implements vscode.TreeDataProvider<c.Cla
             if (hideNonOwnedLabsState) {
                 labs = this.treeItems.filter(labNode => labNode.owner == username);
             } else {
-                labs = this.treeItems.filter(labNode => labNode.owner == username);
+                labs = this.treeItems;
             }
             vscode.commands.executeCommand(
                 'setContext',
