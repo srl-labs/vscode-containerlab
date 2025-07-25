e3fd821501f5a35e6fcc2e51c412dc56c46716c3
flosch62 <schwarz.flori.88@gmail.com>
2025-07-25 16:38:16 +0200

    fix lint and tests

--- test/helpers/utils-stub.ts
+++ test/helpers/utils-stub.ts
@@ -1,3 +1,8 @@
 export function getSudo() {
   return '';
 }
+
+export async function getSelectedLabNode(node?: any): Promise<any> {
+  // In tests, always return the node that was passed in
+  return node;
+}
