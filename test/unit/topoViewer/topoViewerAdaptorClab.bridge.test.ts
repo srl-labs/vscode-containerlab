cee827f8eabde991ac2645ee9211e043c5d94293
flosch62 <schwarz.flori.88@gmail.com>
2025-08-20 17:04:43 +0200

    update test

--- test/unit/topoViewer/topoViewerAdaptorClab.bridge.test.ts
+++ test/unit/topoViewer/topoViewerAdaptorClab.bridge.test.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { TopoViewerAdaptorClab } from '../../../src/topoViewer/core/topoViewerAdaptorClab';
 
 describe('TopoViewerAdaptorClab bridge nodes', () => {
-  it('assigns cloud topoViewerRole for bridge kinds', async () => {
+  it('assigns bridge topoViewerRole for bridge kinds', async () => {
     const adaptor = new TopoViewerAdaptorClab();
     const yaml = `
 name: test
@@ -18,8 +18,8 @@ topology:
     const elements = await adaptor.clabYamlToCytoscapeElementsEditor(yaml);
     const br = elements.find(el => el.data?.id === 'br1');
     const ovs = elements.find(el => el.data?.id === 'ovs1');
-    expect(br?.data?.topoViewerRole).to.equal('cloud');
-    expect(ovs?.data?.topoViewerRole).to.equal('cloud');
+    expect(br?.data?.topoViewerRole).to.equal('bridge');
+    expect(ovs?.data?.topoViewerRole).to.equal('bridge');
   });
 });
 
