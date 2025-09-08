4b37611910b97eac7402187265630b5eb67c1e7c
Kaelem Chandra <kc@kaelem.net>
2025-09-09 04:57:31 +1200

    fix: make node integrated terminal unique

--- src/commands/showLogs.ts
+++ src/commands/showLogs.ts
@@ -9,17 +9,18 @@ export function showLogs(node: ClabContainerTreeNode) {
         return;
     }
     const containerId = node.cID;
-    const containerLabel = node.label || "Container";
 
     if (!containerId) {
         vscode.window.showErrorMessage('No containerID for logs.');
         return;
     }
 
+    const container = node.name || containerId
+
     const config = vscode.workspace.getConfiguration("containerlab");
     const runtime = config.get<string>("runtime", "docker");
     execCommandInTerminal(
         `${getSudo()}${runtime} logs -f ${containerId}`,
-        `Logs - ${containerLabel}`
+        `Logs - ${container}`
     );
 }
