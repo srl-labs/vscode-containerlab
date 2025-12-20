08b130a05d4f5e94e88a4b4958f66068ad20c85a
Flosch62 <schwarz.flori.88@gmail.com>
2025-12-20 15:34:32 +0100

    mac os support

--- src/reactTopoViewer/webview/components/panels/FloatingActionPanel.tsx
+++ src/reactTopoViewer/webview/components/panels/FloatingActionPanel.tsx
@@ -1,5 +1,5 @@
 /**
  * Re-export from floatingPanel module for backwards compatibility
  */
-export { FloatingActionPanel } from './floatingPanel';
-export type { FloatingActionPanelHandle } from './floatingPanel';
+export { FloatingActionPanel } from './floatingPanel/index';
+export type { FloatingActionPanelHandle } from './floatingPanel/index';
